5bb6ffa2a877e10e52e5b4d7c75a7c7a876b2c71
nageshvilaskolhe <kolhenagesh2017@gmail.com>
2023-09-23 16:50:29 +0530

    Added test

--- src/test/java/SampleTest.java
+++ src/test/java/SampleTest.java
@@ -2,17 +2,29 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import io.restassured.RestAssured;
+import io.restassured.RestAssured.*;
 import io.restassured.response.Response;
 import jdk.internal.net.http.ResponseBodyHandlers;
 
 public class SampleTest {
-@Test
+	@Test
 	void test_01() {
 		Response response = RestAssured.get("https://reqres.in//api/users?page=2");
-		
+
 		response.getBody();
 		response.getHeaders();
 		int Res = response.statusCode();
-		Assert.assertEquals(Res,404);
+		Assert.assertEquals(Res, 404);
+	}
+
+	@Test
+	public void test_2() {
+		given();
+		RestAssured.get("https://reqres.in/api/users?page=2").then().statusCode(200).log().all();
+	}
+
+	private RestAssured given() {
+		// TODO Auto-generated method stub
+		return null;
 	}
 }
