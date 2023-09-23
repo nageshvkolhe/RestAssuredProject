601a56d270cd58d85a269f94a03857393dce9d71
nageshvilaskolhe <kolhenagesh2017@gmail.com>
2023-09-23 18:35:18 +0530

    Updated post method

--- src/test/java/Tests_POST.java
+++ src/test/java/Tests_POST.java
@@ -4,18 +4,34 @@ import java.util.Map;
 import org.json.simple.JSONObject;
 import org.testng.annotations.Test;
 
-import io.restassured.internal.path.json.mapping.JsonObjectDeserializer;
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
 
 public class Tests_POST {
 	@Test
 	public void test_post() {
-		Map<String, Object> map = new HashMap<String, Object>();
-		
-		map.put("name", "Nagesh");
-		map.put("job", "SDET");
+//		Map<String, Object> map = new HashMap<String, Object>();
+//		
+//		map.put("name", "Nagesh");
+//		map.put("job", "SDET");
+//
+//		System.out.println(map);
+		JSONObject request = new JSONObject();
 
-		System.out.println(map);
-		JSONObject req = new JSONObject(map);
-		System.out.println(req);
+		request.put("name", "Nagesh");
+		request.put("job", "SDET");
+
+		System.out.println(request);
+		System.out.println(request.toJSONString());
+
+		given()
+		.header("Content-Type","application/json")
+		.contentType(ContentType.JSON)
+		.body(request.toJSONString())
+		.when()
+		.post("https://reqres.in/api/users").then().statusCode(201);
 	}
 }
