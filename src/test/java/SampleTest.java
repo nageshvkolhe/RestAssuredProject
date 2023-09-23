b874adf5836f60f15c94dfbeffd26ba948ee1df3
nageshvilaskolhe <kolhenagesh2017@gmail.com>
2023-09-23 17:06:01 +0530

    Updated test

--- src/test/java/SampleTest.java
+++ src/test/java/SampleTest.java
@@ -2,9 +2,9 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import io.restassured.RestAssured;
-import io.restassured.RestAssured.*;
+import io.restassured.matcher.ResponseAwareMatcher;
 import io.restassured.response.Response;
-import jdk.internal.net.http.ResponseBodyHandlers;
+import static org.hamcrest.Matchers.*;
 
 public class SampleTest {
 	@Test
@@ -20,7 +20,9 @@ public class SampleTest {
 	@Test
 	public void test_2() {
 		given();
-		RestAssured.get("https://reqres.in/api/users?page=2").then().statusCode(200).log().all();
+		RestAssured.get("https://reqres.in/api/users?page=2").then().statusCode(200).body("data.id[1]", equalTo(8))
+//		body("data.id[1]", hasItems())
+				.log().all();
 	}
 
 	private RestAssured given() {
